e2a2d48525e376ffa7aa62da49c9692c3dd19000
Gonzalo Otero Lobo <gonzalo.chalo@gmail.com>
2025-04-28 12:35:11 -0500

    Revert "make script executable"
    This reverts commit f13d14f807eef1750e61a17bbf579f4adc1ee5f1.

--- video-streaming/src/main/java/com/example/demo/InMemoryVideoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-
-import org.springframework.context.annotation.Profile;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-
-public interface InMemoryVideoRepository extends CrudRepository<VideoData, String> {
-}
